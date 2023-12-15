288dde8b3e6fe7632b4547816ea19e664d723b38
kiko-pan <146799618+kiko-pan@users.noreply.github.com>
2023-12-14 17:32:17 -0800

    Delete COGS3Final directory

--- COGS3Final/portfolio.js
+++
@@ -1,7 +0,0 @@
-function success(event){
-    event.preventDefault()
-    document.getElementById("overlay").style.display = "block";
-    setTimeout(() => {
-        document.getElementById('overlay').style.display = 'none';
-    }, 1000);
-}
